b2f597f844f48e435eb841560088b52f60bc1d77
meire-zup <jucemeire.lopes@zup.com.br>
2023-06-15 22:57:08 -0300

    finalizacao exercicio1 classe abstrata

--- src/exercicio1_CA/Loja.java
+++ src/exercicio1_CA/Loja.java
@@ -11,9 +11,11 @@ public class Loja {
         //Instanciando produtos:
         Produto livro1 = new Livro("1984", 80.00, "autor", 123456L);
         Produto cd1 = new Cd("21", 50.90, 5, 123457L);
-        Produto dvd = new Dvd("Dvd", 40.00, 55, 123458L);
+        Produto dvd = new Dvd("Dvd", 40.00, 55, 123477L);
         Produto livro2 = new Livro("O Senhor dos Anéis", 200.50, "autor", 123459L);
         Produto cd2 = new Cd("Thriller", 75.60, 4, 123450L);
+        Produto livro3 = new Livro("1984", 80.00, "autor", 123456L);
+        Produto cd3 = new Cd("21", 50.90, 5, 123457L);
 
         //Adicionando produtos:
 
@@ -22,6 +24,8 @@ public class Loja {
         produtos.add(cd2);
         produtos.add(livro2);
         produtos.add(dvd);
+        produtos.add(livro3);
+        produtos.add(cd3);
 
         //Imprimindo produtos:
         for ( Produto produto : produtos) {
@@ -31,17 +35,30 @@ public class Loja {
 
         }
 
+        // Comparando produtos:
+        System.out.println("COMPARANDO PRODUTOS:");
+        compararProdutos(livro1, livro3);
+        System.out.println("********************************");
 
-        System.out.println(produtos.indexOf(livro1));
+        // Buscando produto:
+        System.out.println("BUSCANDO PRODUTOS:");
+        buscarProduto(produtos,123477L);
 
-        Produto produto1 = livro1;
-        Produto produto2 = livro2;
-
+    }
+    public static void compararProdutos(Produto produto1, Produto produto2){
         if(produto1.equals(produto2)){
             System.out.println("Produtos são iguais");
         } else {
             System.out.println("Produtos diferentes.");
         }
+    }
 
+    public static void buscarProduto(List<Produto> produtos, Long codigoDeBarras){
+        for (Produto produto : produtos) {
+            if(produto.getCoodigoDeBarras().equals(codigoDeBarras)){
+                System.out.println("Produto encontrado na posição: "+produtos.indexOf(produto));
+            }
+        }
     }
 }
+
